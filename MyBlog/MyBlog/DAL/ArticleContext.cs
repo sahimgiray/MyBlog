491b3557ad2022a59032adea18339f52cab11424
sahimgiray <sahimgiray94@gmail.com>
2015-05-31 11:38:53 +0300

    Model and Article Controller is added

---
+++ MyBlog/MyBlog/DAL/ArticleContext.cs
@@ -0,0 +1,16 @@
+﻿using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyBlog.DAL
+{
+    public class ArticleContext:DbContext
+    {
+        public ArticleContext() : base("ArticleContext") { }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
---
+++ MyBlog/MyBlog/Models/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyBlog.Models
+{
+    public class Article
+    {
+        public Article()
+        {
+
+        }
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Author { get; set; }
+        [DataType(DataType.Html)]
+        public string Content { get; set; }
+
+    }
+}
