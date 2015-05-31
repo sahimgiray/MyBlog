e0e45dc8d6b9950caa27275134ffe25f9d531f4e
sahimgiray <sahimgiray94@gmail.com>
2015-05-31 12:53:03 +0300

    Changed About and Home Index Views

--- MyBlog/MyBlog/Controllers/HomeController.cs
+++ MyBlog/MyBlog/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MyBlog.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Şahım Giray Kıvanç";
 
             return View();
         }
